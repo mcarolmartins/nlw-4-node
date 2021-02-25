a92c61bf1fbf0bb1259d9388792e1084f579df52
Maria Carolina <carol-martins7@hotmail.com>
2021-02-24 22:40:49 -0300

    adicionando repositório, models e controllers de surveys

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,31 +1,31 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
-import { User } from "../models/User";
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
 
 class UserController {
-
+  
   async create(req: Request, resp: Response){
     const {name, email} = req.body;
-
-    const usersRepository = getRepository(User);
-
+    
+    const usersRepository = getCustomRepository(UsersRepository);
+    
     const userAlreadyExists = await usersRepository.findOne({email});
-
+    
     if(userAlreadyExists){
       return resp.status(400).json({
         error: "user already exists!"
       });
     }
-
+    
     const user = usersRepository.create({
       name, email
     });
-
+    
     await usersRepository.save(user);
-
+    
     return resp.json(user);
   }
-
+  
 }
 
-export {UserController};
+export { UserController };
