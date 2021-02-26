7ca737dfc458acb366780b37278d67ff7ed0259f
Maria Carolina <carol-martins7@hotmail.com>
2021-02-26 17:11:24 -0300

    calculo de NPS

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,20 +1,31 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { UsersRepository } from "../repositories/UsersRepository";
+import * as yup from 'yup';
+import { AppError } from "../errors/AppError";
 
 class UserController {
   
   async create(req: Request, resp: Response){
     const {name, email} = req.body;
-    
+
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+    });
+
+    try {
+      await schema.validate(req.body, { abortEarly: false })
+    } catch (err) {
+      throw new AppError(err);
+    }
+
     const usersRepository = getCustomRepository(UsersRepository);
     
     const userAlreadyExists = await usersRepository.findOne({email});
     
     if(userAlreadyExists){
-      return resp.status(400).json({
-        error: "user already exists!"
-      });
+      throw new AppError("User already exists!");
     }
     
     const user = usersRepository.create({
@@ -24,6 +35,14 @@ class UserController {
     await usersRepository.save(user);
     
     return resp.status(201).json(user);
+  }
+
+  async show(req: Request, resp: Response){
+    const usersRepository = getCustomRepository(UsersRepository);
+    
+    const all = await usersRepository.find();
+
+    return resp.json(all);
   }
   
 }
